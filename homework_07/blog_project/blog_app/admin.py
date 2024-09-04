05ab0e68dc7fbf09e220e123073f0434a2bd38b9
fullwaffle <yefremenko@aq.ru>
2024-09-04 18:41:10 +0300

    otus homework #07 fixes

--- homework_07/blog_project/blog_app/admin.py
+++ homework_07/blog_project/blog_app/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 
-from .models import Post, Comments
+from .models import Post, Comment
+
+
+class CommentTabularInline(admin.TabularInline):
+    model = Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentTabularInline]
     list_display = "id", "title", "created_on"
     list_display_links = "id", "title"
 
 
-@admin.register(Comments)
-class CommentsAdmin(admin.ModelAdmin):
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     list_display = "id", "author", "post", "content"
     list_display_links = "id", "content"
--- homework_07/blog_project/blog_app/models.py
+++ homework_07/blog_project/blog_app/models.py
@@ -12,20 +12,20 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_on = models.DateField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
     def __str__(self):
         return self.title
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     class Meta:
         verbose_name = "Комментарий"
         verbose_name_plural = "Комментарии"
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
 
     def __str__(self):
         return f"{self.author}, {self.post}, {self.content}"
